107e362a5482d836c843be752e0e89f3ac2180d7
Rohit <rohit.mmm1996@gmail.com>
2021-07-27 00:42:53 +0530

    Added Meta Info

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,28 @@
+import Head from "next/head";
+
 import { ThemeProvider } from "../Context/ThemeContext";
 import { Container } from "../components/Container";
 
 export default function Home() {
   return (
-    <ThemeProvider>
-      <div className="bg-gray-300 dark:bg-black">
-        <Container />
-      </div>
-    </ThemeProvider>
+    <>
+      <Head>
+        <title>Easy Search</title>
+        <meta
+          name="description"
+          content="Get better search results from Google by limiting sites, file extensions, dates and more"
+        />
+        <meta
+          name="keywords"
+          content="Google search, Advanced Search, Accurate Search"
+        />
+        <meta name="author" content="Rohit Kashyap" />
+      </Head>
+      <ThemeProvider>
+        <div className="bg-gray-300 dark:bg-black">
+          <Container />
+        </div>
+      </ThemeProvider>
+    </>
   );
 }
